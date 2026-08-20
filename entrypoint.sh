c1ba702f6a9c85ce126f934598a3d102116acb08
Werner Raatz <werner.raatz@mail.de>
2026-08-20 16:34:40 +0200

    feat(docker): create superuser from env vars on container start
    Non-interactive createsuperuser reads DJANGO_SUPERUSER_USERNAME/
    EMAIL/PASSWORD, letting a fresh container get an admin account without
    baking credentials into the image or committing them to git. Ignores
    failure so restarts against an existing user don't break startup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py createsuperuser --noinput || true
+
 # APP_PORT variable must be present in env
 python manage.py runserver 0.0.0.0:${APP_PORT}
